6330b9e6ea346c53e700fd2fb27b90f28305b21d
okiemuteidjerhe <kiemute.io@gmail.com>
2025-04-18 04:24:10 -0700

    Added functionality to the works buttont, added a smoother effect to the mobile nav and hamburger and corrected the color of the font in .cust-email

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+//The hamburger
 let mobileNav = document.querySelector('.mobile-view');
 let hamburger = document.querySelector('.hideOnMain');
 
 const toggleMobileNav = () => {
     hamburger.classList.toggle('close');
-    
-    if(hamburger.classList.contains('close')){
-        mobileNav.style.display = 'block';
-    }else{
-        mobileNav.style.display = 'none';
-    }
+    mobileNav.classList.toggle('reveal-nav');
 }
 
 hamburger.addEventListener('click', toggleMobileNav);
 mobileNav.addEventListener('click', toggleMobileNav);
+
+//The works button
+let toggleBtn = document.getElementById("see");
+let hiddenImages = document.getElementsByClassName("hide-img");
+
+toggleBtn.addEventListener(
+    "click",
+     ()=>{
+        for(image of hiddenImages){
+            image.classList.toggle("not-hidden");
+        };
+     
+     if (hiddenImages[0].classList.contains('not-hidden')){
+        toggleBtn.textContent = "See Less";
+     }else{
+        toggleBtn.textContent = "See More";
+     }
+    }
+    )
