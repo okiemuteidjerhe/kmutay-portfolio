a99c4dc80359bdba2a22341bb1638b5c4965369c
okiemuteidjerhe <kiemute.io@gmail.com>
2026-01-30 08:29:18 +0100

    Integrated EmailJS and a response

--- script.js
+++ script.js
@@ -28,3 +28,46 @@ toggleBtn.addEventListener(
      }
     }
     )
+
+//The Year
+   const currentYear = document.querySelector('time')
+
+   const yearNow = () => {
+      const dateNow = new Date().getFullYear();
+      currentYear.textContent = dateNow;
+   }
+   yearNow();
+
+//Send Email
+   const cEmail = document.querySelector('.cust-email')
+   const form = document.querySelector('.email-ctn')
+   const dialog = document.querySelector('.message')
+   const title = document.querySelector('.status-title')
+   const message = document.querySelector('.status-message')
+   const dialogBtn = document.querySelector('#dialogBtn')
+
+   const sendMail = (e) => {
+      e.preventDefault();
+
+      const params = {
+         cust_email: cEmail.value
+      }
+
+      emailjs.send('service_tn848ze', 'template_tpnmi34', params).then(
+         (response) => {
+            title.textContent = 'Email Sent'
+            message.textContent = 'We wil get back to you shortly!'
+            cEmail.value = ''
+            dialog.showModal();
+         },
+         (error) => {
+            title.textContent = 'Message Failed'
+            message.textContent = 'Something went wrong. Please retry.'
+            cEmail.value = ''
+            dialog.showModal();
+         },
+      );
+   }
+
+   form.addEventListener("submit", sendMail)
+   dialogBtn.addEventListener('click', () => dialog.close())
